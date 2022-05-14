877902a61f2c812c8ba968b14a6ba098b40efb53
sreyasqb <20pt33@psgtech.ac.in>
2022-05-14 22:57:39 +0530

    app created

---
+++ android/app/src/main/kotlin/com/example/nvld_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.nvld_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) => MyApp(), // Wrap your app
+    ),
+    // MyApp()
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(),
+      useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        
+          
+
+          ),
+    );
+  }
+}
