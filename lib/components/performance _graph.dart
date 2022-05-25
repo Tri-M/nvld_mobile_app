8d597a5a9fe309bd7ec8a26bf12547bdd8b3aefb
Karthik <kmkarthik97@gmail.com>
2022-05-25 16:19:26 +0530

    graph gradient

--- lib/components/performance _graph.dart
+++ lib/components/performance _graph.dart
@@ -14,18 +14,35 @@ class PerformanceGraph extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final List<ChartData> chartData = [
-      ChartData(2010, 35),
-      ChartData(2011, 28),
-      ChartData(2012, 34),
-      ChartData(2013, 32),
-      ChartData(2014, 40)
+      ChartData(1924, 12),
+      ChartData(1925, 7),
+      ChartData(1926, 5),
+      ChartData(1927, 10),
+      ChartData(1928, 10),
+      ChartData(1929, 13),
+      ChartData(1930, 5),
     ];
-    return SfCartesianChart(series: <ChartSeries>[
-      // Renders area chart
-      AreaSeries<ChartData, int>(
-          dataSource: chartData,
-          xValueMapper: (ChartData data, _) => data.x,
-          yValueMapper: (ChartData data, _) => data.y)
-    ]);
+    final List<Color> color = <Color>[];
+    color.add(Color.fromARGB(255, 203, 180, 226)!);
+    color.add(Color.fromARGB(255, 175, 129, 229)!);
+    color.add(Color.fromARGB(255, 110, 0, 206));
+
+    final List<double> stops = <double>[];
+    stops.add(0.0);
+    stops.add(0.5);
+    stops.add(1.0);
+
+    final LinearGradient gradientColors =
+        LinearGradient(colors: color, stops: stops);
+    return SfCartesianChart(
+        primaryYAxis: NumericAxis(labelFormat: '{value}'),
+        series: <ChartSeries>[
+          // Renders area chart
+          AreaSeries<ChartData, int>(
+              dataSource: chartData,
+              xValueMapper: (ChartData data, _) => data.x,
+              yValueMapper: (ChartData data, _) => data.y,
+              gradient: gradientColors)
+        ]);
   }
 }
--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -44,29 +44,37 @@ class _DashboardState extends State<Dashboard> {
             child: Column(
               children: [
                 Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Container(
                       padding: EdgeInsets.only(
-                          top: height * 0.035,
-                          bottom: height * 0.03,
-                          left: width * 0.06),
+                        top: height * 0.035,
+                        bottom: height * 0.03,
+                        left: width * 0.06,
+                      ),
                       alignment: Alignment.topLeft,
                       child: TextContainer(
                         text: "Hello user!",
                         presetFontSizes: const [24, 22, 20, 18, 16],
                         textAlign: TextAlign.left,
-                        width: width * 0.6,
+                        //width: width * 0.6,
                         style: const TextStyle(
                           color: Colors.black,
                           fontWeight: FontWeight.bold,
                         ),
                       ),
                     ),
-                    TextContainer(
-                      text: "Level 3",
-                      presetFontSizes: [20, 18, 16, 14],
-                    )
+                    Container(
+                      padding: EdgeInsets.only(
+                        top: height * 0.035,
+                        bottom: height * 0.03,
+                        right: width * 0.06,
+                      ),
+                      child: TextContainer(
+                        text: "Level 3",
+                        presetFontSizes: [20, 18, 16, 14],
+                      ),
+                    ),
                   ],
                 ),
                 Card(
@@ -75,27 +83,19 @@ class _DashboardState extends State<Dashboard> {
                     borderRadius: BorderRadius.circular(15),
                   ),
                   child: Container(
-                    child: PerformanceGraph(),
+                    child: Container(
+                        padding: EdgeInsets.only(
+                          top: height * 0.01,
+                          bottom: height * 0.01,
+                          right: width * 0.06,
+                          left: width * 0.02,
+                        ),
+                        child: PerformanceGraph()),
                     height: height * 0.2,
                     width: width * 0.75,
                     decoration: BoxDecoration(
-                      gradient: const LinearGradient(
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
-                        colors: [
-                          Color.fromRGBO(159, 108, 242, 1),
-                          Color.fromRGBO(88, 57, 178, 1),
-                        ],
-                      ),
                       borderRadius: BorderRadius.circular(15),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(0.5),
-                          spreadRadius: width * 0.01,
-                          blurRadius: 4,
-                          offset: const Offset(0, 3),
-                        ),
-                      ],
+                      color: const Color.fromRGBO(213, 213, 213, 1),
                     ),
                   ),
                 ),
@@ -116,19 +116,28 @@ class _DashboardState extends State<Dashboard> {
                               TextContainer(
                                   text: "Recent Score",
                                   presetFontSizes: [18, 16, 14, 12],
-                                  style:
-                                      TextStyle(fontWeight: FontWeight.bold)),
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white)),
                               TextContainer(
                                   text: "13/15",
                                   presetFontSizes: [20, 18, 16, 14, 12],
-                                  style:
-                                      TextStyle(fontWeight: FontWeight.bold)),
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white)),
                             ],
                           ),
                           height: width * 0.36,
                           width: width * 0.36,
                           decoration: BoxDecoration(
-                            color: const Color.fromRGBO(213, 213, 213, 1),
+                            gradient: const LinearGradient(
+                              begin: Alignment.topCenter,
+                              end: Alignment.bottomCenter,
+                              colors: [
+                                Color.fromRGBO(159, 108, 242, 1),
+                                Color.fromRGBO(88, 57, 178, 1),
+                              ],
+                            ),
                             borderRadius: BorderRadius.circular(15),
                           ),
                         ),
@@ -143,20 +152,31 @@ class _DashboardState extends State<Dashboard> {
                             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                             children: [
                               TextContainer(
-                                  text: "Best Score",
-                                  presetFontSizes: [18, 16, 14, 12],
-                                  style:
-                                      TextStyle(fontWeight: FontWeight.bold)),
+                                text: "Best Score",
+                                presetFontSizes: [18, 16, 14, 12],
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white),
+                              ),
                               TextContainer(
                                   text: "14/15",
                                   presetFontSizes: [20, 18, 16, 14, 12],
-                                  style: TextStyle(fontWeight: FontWeight.bold))
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white))
                             ],
                           ),
                           height: width * 0.36,
                           width: width * 0.36,
                           decoration: BoxDecoration(
-                            color: const Color.fromRGBO(213, 213, 213, 1),
+                            gradient: const LinearGradient(
+                              begin: Alignment.topCenter,
+                              end: Alignment.bottomCenter,
+                              colors: [
+                                Color.fromRGBO(159, 108, 242, 1),
+                                Color.fromRGBO(88, 57, 178, 1),
+                              ],
+                            ),
                             borderRadius: BorderRadius.circular(15),
                           ),
                         ),
