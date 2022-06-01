61e63385d08251c96acfea66af5f3bb4086a4403
sreyasqb <20pt33@psgtech.ac.in>
2022-06-01 10:19:10 +0530

    few changes

---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+// import '../components/body_welcome.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Body(),
+    );
+  }
+}
